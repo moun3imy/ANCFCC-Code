bc51f0d714f1cfb7435426f3ab88b5c9215567c2
mounaim <mounaim.zaryouhi@gmail.com>
2017-11-03 23:21:37 +0000

    Update perceptron.c
    removed unuseful comments

--- perceptron.c
+++ perceptron.c
@@ -32,8 +32,6 @@ int CalculerSortie(float poids[],int x[])
 }
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    //
     char* file_path = "path_to_file";
     FILE* fp ;
     if((fp = fopen(file_path,"r"))==NULL){
